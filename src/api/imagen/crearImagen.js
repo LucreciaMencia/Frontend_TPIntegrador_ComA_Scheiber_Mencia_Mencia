e279ae27c7719157cdd7e261f78ad0fe9ddaf16f
Lucrecia <mencialucre@gmail.com>
2023-10-23 17:32:58 -0300

    MM-44 | Cambios en crearImagen

--- src/api/imagen/crearImagen.js
+++ src/api/imagen/crearImagen.js
@@ -1,3 +1,12 @@
-export default function crearImagen() {
-    
+export default function crearImagen(imagen) {
+    const data = new FormData();
+    data.append('url-imagen', imagen.url);
+    data.append('nombre', imagen.name);
+
+    let parametros = {
+        method: 'POST',
+        body: data
+    }
+
+    return fetch("http://localhost:8080/comida/imagen", parametros)
 }
